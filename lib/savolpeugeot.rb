aaac26fe093fff3b8b889c0d3045ff59db4c6c0f
Thiago Youssef <thiagodyoussef@gmail.com>
2022-01-20 13:21:31 -0300

    Add filter for lead products

--- lib/savolpeugeot.rb
+++ lib/savolpeugeot.rb
@@ -6,5 +6,25 @@ require "f1sales_helpers"
 
 module Savolpeugeot
   class Error < StandardError; end
-  # Your code goes here...
+
+  class F1SalesCustom::Hooks::Lead
+    class << self
+      def switch_source(lead)
+        product_name = lead.product ? lead.product.name : ''
+        source_name = lead.source ? lead.source.name : ''
+
+        if product_name.downcase.include?('sbc')
+          "#{source_name} - TIME SBC USADOS"
+        elsif product_name.downcase.include?('sto')
+          "#{source_name} - TIME STO USADOS"
+        elsif product_name.downcase.include?('citroën')
+          "#{source_name} - TIME CITROEN"
+        elsif product_name.downcase.include?('peugeot')
+          "#{source_name} - TIME PEUGEOT"
+        else
+          lead.source.name
+        end
+      end
+    end
+  end
 end
