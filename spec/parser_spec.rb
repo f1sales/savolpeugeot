efef455c599a43c8873f5f7a4397bf6f88fa8139
Nathanael Vieira <nathanael@f1sales.com.br>
2022-04-26 14:40:39 -0300

    remove !@# from email

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -42,4 +42,45 @@ RSpec.describe F1SalesCustom::Email::Parser do
       expect(parsed_email[:description]).to eq('Campanha: Ofertas - Savol Fiat -; Origem: Facebook; Loja: Wandalla.monteiro@savol.com.br')
     end
   end
+
+  context 'when is from website and has !@#' do
+    let(:email) do
+      email = OpenStruct.new
+      email.to = [email: 'website-seminovos@savolkia.f1sales.org']
+      email.subject = 'Oferta / Estoque - Proposta'
+      email.body = "!@\#{\n\"CidadeRevenda\":\"Savol Kia (São Bernardo do Campo)\",\n\"Veículo\":\"Sportage \",\n\"Placa\":\"OLL8751\",\n\"Preço\":\"R$ 69.900,00\",\n\"Nome\": \"Guilherme\",\n\"E-mail\": \"guilima@me.com\",\n\"Telefone\": 11998108688,\n\"Descricao\": \"Olá tenho Kia Cerato 2017 e tenho interesse na troca.\"\n}!@#"
+
+      email
+    end
+
+    let(:parsed_email) { described_class.new(email).parse }
+
+    it 'contains lead website a source name' do
+      expect(parsed_email[:source][:name]).to eq(F1SalesCustom::Email::Source.all[0][:name])
+    end
+
+    it 'contains name' do
+      expect(parsed_email[:customer][:name]).to eq('Guilherme')
+    end
+
+    it 'contains email' do
+      expect(parsed_email[:customer][:email]).to eq('guilima@me.com')
+    end
+
+    it 'contains phone' do
+      expect(parsed_email[:customer][:phone]).to eq('11998108688')
+    end
+
+    it 'contains message' do
+      expect(parsed_email[:message]).to eq('Olá tenho Kia Cerato 2017 e tenho interesse na troca.')
+    end
+
+    it 'contains product' do
+      expect(parsed_email[:product][:name]).to eq('Sportage OLL8751')
+    end
+
+    it 'contains description' do
+      expect(parsed_email[:description]).to eq('Preço R$ 69.900,00')
+    end
+  end
 end
